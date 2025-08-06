df3e79776908a0324c46b064f8cc533f2e61db35
tizzifona <tizzifona@gmail.com>
2025-08-06 11:20:25 +0200

    chore: initial project setup with dependencies

---
+++ src/main/java/femcoders25/mykitchen_hub/MyKitchenHubApplication.java
@@ -0,0 +1,13 @@
+package femcoders25.mykitchen_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyKitchenHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyKitchenHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/femcoders25/mykitchen_hub/MyKitchenHubApplicationTests.java
@@ -0,0 +1,13 @@
+package femcoders25.mykitchen_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyKitchenHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
